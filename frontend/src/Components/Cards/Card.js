d7e1197ce327b26da92a8ce82a458b767ede3543
Karina <karinakarpenko.k@gmail.com>
2021-04-10 13:44:06 +0300

    images

--- frontend/src/Components/Cards/Card.js
+++ frontend/src/Components/Cards/Card.js
@@ -7,14 +7,19 @@ export default function CardItem(props){
     const { product } = props
     return(
         <Col>
-            <Card style={{ width: '18rem'}} key={product._id}>
-                <Card.Img variant="top" src={product.image} />
-                <Card.Body>
+            <Card style={{ width: '18rem',
+                            height: '30rem'}} key={product._id}>
+                <Card.Img variant="top" src={product.image} rounded/>
+                <Card.Body style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between'
+                            }}>
                     <Card.Title>{product.name}</Card.Title>
                     <Card.Text>
                     {product.description}
                     </Card.Text>
-                    <Link to={`/product/${product._id}`}><Button variant="outline-info"> Go somewhere</Button></Link>
+                    <Link to={`/product/${product._id}`}><Button variant="outline-info" style={{float: 'right'}}> Go somewhere</Button></Link>
                 </Card.Body>
             </Card>
         </Col>
--- frontend/src/Components/Slider.js
+++ frontend/src/Components/Slider.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import photo from '../images/s1.jpg'
-import photo1 from '../images/s1.jpg'
-import photo2 from '../images/s1.jpg'
+import photo1 from '../images/s-1.jpg'
+import photo2 from '../images/s-1.jpg'
+import photo3 from '../images/s-1.jpg'
+import photo4 from '../images/s-1.jpg'
 
 export default function Slider(){
     return(
         <Carousel>
-            <Carousel.Item style={{'height': '50vh'}}>
-                <img 
-                    className="d-block w-100"
-                    src={photo}
-                    alt="First Slide"
-                />
-                <Carousel.Caption>
-                    <h3>Heading</h3>
-                    <p>Lorem ipsum</p>
-                </Carousel.Caption>
-            </Carousel.Item>
             <Carousel.Item style={{'height': '50vh'}}>
                 <img 
                     className="d-block w-100"
@@ -33,6 +23,28 @@ export default function Slider(){
                 <img 
                     className="d-block w-100"
                     src={photo2}
+                    alt="First Slide"
+                />
+                <Carousel.Caption>
+                    <h3>Heading</h3>
+                    <p>Lorem ipsum</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item style={{'height': '50vh'}}>
+                <img 
+                    className="d-block w-100"
+                    src={photo3}
+                    alt="First Slide"
+                />
+                <Carousel.Caption>
+                    <h3>Heading</h3>
+                    <p>Lorem ipsum</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item style={{'height': '50vh'}}>
+                <img 
+                    className="d-block w-100"
+                    src={photo4}
                     alt="First Slide"
                 />
                 <Carousel.Caption>
