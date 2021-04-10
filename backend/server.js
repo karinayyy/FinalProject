babfc6d9cfd833190f442e95e2151b49806cc5a8
Karina <karinakarpenko.k@gmail.com>
2021-04-11 00:36:00 +0300

    easy node app

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ app.get('/api/products', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Server is ready');
 })
+
+const port = process.env.PORT || 5000;
+
 app.listen(5000, () => {
-    console.log('Server at 5000');
+    console.log(`Server at http://localhost:${port}`);
 }) 
