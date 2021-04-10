3c511e90c75b54346561c1a2ba9f5c0a65040fdd
Karina <karinakarpenko.k@gmail.com>
2021-04-11 01:35:56 +0300

    products from server

--- frontend/src/Components/Cards/CardList.js
+++ frontend/src/Components/Cards/CardList.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import CardItem from './Card'
-import cards from './Cards'
 import { Container, Row } from 'react-bootstrap'
 
 export default function CardList(){
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        const fetchData = async () =>{
+            const { data } = await axios.get('/api/products')
+            setProducts(data)
+        }
+        fetchData()
+    }, [])
     return(
         <Container >
             <Row lg={4} md={3} sm={1}>
-                {cards.products.map((product) => (
+                {products.map((product) => (
                     <CardItem key={product._id} product={product} />
                 ))}
             </Row>
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Slider from '../Components/Slider';
 import CardList from '../Components/Cards/CardList'
-import { Product } from './Product';
-import cards from '../Components/Cards/Cards'
 
 export const Home = () => {
     return (
