aed68f023c526bd971ef2bb0b7497aa1614df06e
Karina <karinakarpenko.k@gmail.com>
2021-04-16 01:23:01 +0300

    products mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const data = require('./data.js');
+// const data = require('./data.js');
+const productRouter = require('./routes/productRouter');
 const userRouter = require('./routes/userRouter');
 
 const app = express();
@@ -10,21 +11,22 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/fullstack', {
     useCreateIndex: true
 })
 
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find( (x) =>x._id === req.params.id )
-    if(product){
-        res.send(product)
-    }else{
-        res.status(404).send({ message: 'Product Not Found' })
-    }
-})
+// app.get('/api/products/:id', (req, res) => {
+//     const product = data.products.find( (x) =>x._id === req.params.id )
+//     if(product){
+//         res.send(product)
+//     }else{
+//         res.status(404).send({ message: 'Product Not Found' })
+//     }
+// })
 
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-})
+// app.get('/api/products', (req, res) => {
+//     res.send(data.products);
+// })
 
 app.use('/api/users', userRouter)
+app.use('/api/products', productRouter)
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
