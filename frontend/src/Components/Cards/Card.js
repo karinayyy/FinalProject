fff1e26c84e42e3939b8daa7eb93a4e18a9baaea
karpenko-karina <karinakarpenko.k@gmail.com>
2021-04-17 00:13:35 +0300

    some styles

--- frontend/src/Components/Cards/Card.js
+++ frontend/src/Components/Cards/Card.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 
 
 export default function CardItem(props){
-    const { product } = props
+    const {product} = props
     return(
             <Col>
                 <Card  key={product._id}>
